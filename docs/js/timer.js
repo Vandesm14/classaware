5e3da7840d035bf9c3d427d354d37e3f9e9f541b
Vandesm14 <vandesm14@hyperjs-servers.com>
2019-08-07 00:38:06 -0400

    beta timer algorithm

--- docs/js/timer.js
+++ docs/js/timer.js
@@ -1,75 +1,11 @@
-var periods = 6;
-// var time = -1;
-var time = -1;
-var maxTime = 60;
-var periodLength = maxTime / periods;
-
-// increaseTimer();
-var timerInterval = setInterval(increaseTimer, 1000);
-
-function increaseTimer() {
-	let pointerWidth = $('.timer-pointer').width();
-	let timerWidth = $('.timer').outerWidth() - pointerWidth;
-	let vw = $(window).width() / 100;
-	
-	let cellSpacing = 44.7;
-	let cellWidth = 10.6;
-
-	time++;
-	if (time > maxTime) {
-		clearInterval(timerInterval);
-		return;
-	}
-
-	if ($(window).width() <= 640) {
-		cellSpacing = 34;
-		cellWidth = 32;
-	}
-
-	// Set time left
-	$('.timer-pointer-label').text(((Math.floor(time / periodLength) + 1) * periodLength) - time);
-
-	// Move pointer
-	$('.timer-pointer').animate({
-		left: ((vw * cellSpacing) + (timerWidth / maxTime) * (time % periodLength)) - (pointerWidth / 2)
-	}, 500);
-
-	// Move timer
-	if (time < periodLength * periods) {
-		$('.timer').animate({
-			left: (vw * cellSpacing) - ((vw * cellWidth) * (Math.floor(time / periodLength) - 0))
-		}, 500);
-	}
-
-	// Update columns
-	if (time % periodLength === 0 || time === 0 && time < periodLength * (periods - 1)) {
-
-		// Reset column classes
-		$('.timer').children('.timer-column').removeClass('current');
-		$('.timer').children('.timer-column').css('opacity', '0.2');
-
-		// Set main column class
-		$('.timer').children(`.timer-column:eq(${Math.floor(time / periodLength)})`).addClass('current');
-		$('.timer').children(`.timer-column:eq(${Math.floor(time / periodLength)})`).css('opacity', '1');
-
-		// Show secondary columns
-		$('.timer').children(`.timer-column:eq(${Math.floor(time / periodLength) + 1})`).css('opacity', '1');
-		if ((Math.floor(time / periodLength) - 1) !== -1) {
-			$('.timer').children(`.timer-column:eq(${Math.floor(time / periodLength) - 1})`).css('opacity', '1');
-		}
-	}
-}
-
-function updateTimer() {
-
-}
-
+/* jshint esversion: 6 */
 var schedules = [{
 		name: 'normal',
 		days: '35',
 		data: {
 			times: ['7:50', '8:40', '9:30', '10:20', '11:10', '12:00'],
-			names: ['Period 1', 'Period 2', 'Period 3', 'Period 4', 'Period 5', 'Period 6']
+			names: ['Period 1', 'Period 2', 'Period 3', 'Period 4', 'Period 5', 'Period 6'],
+			short: ['1', '2', '3', '4', '5', '6']
 		}
 	},
 	{
@@ -77,23 +13,140 @@ var schedules = [{
 		days: '124',
 		data: {
 			times: ['7:50', '8:40', '9:30'],
-			names: ['Period 1', 'Period 2', 'Period 3']
+			names: ['Period 1', 'Period 2', 'Period 3'],
+			short: ['1', '2', '3']
 		}
 	}
 ];
 
+var time = -1;
+var maxTime = 360;
+var periodLength = maxTime / periods;
+var remainingTime;
+
+var scheduleIndex;
+var currentSchedule;
+var periods;
+var currentPeriod;
+
+var timerInterval;
+var timeOffset = 28800;
+
+getCurrentSchedule();
+
+function increaseTimer() {
+	let pointerWidth = $('.timer-pointer').width();
+	let timerWidth = $('.timer').outerWidth() - pointerWidth;
+	let vw = $(window).width() / 100;
+
+	let cellSpacing = 45;
+	let cellWidth = 10;
+
+	time++;
+	getCurrentPeriod();
+	if (time > maxTime) {
+		clearInterval(timerInterval);
+		return;
+	}
+
+	if ($(window).width() <= 640) {
+		cellSpacing = 32;
+		cellWidth = 30;
+	}
+
+	// Update info
+	$('.info-date').text(new Date().toLocaleString().split(', ')[0]);
+	$('.info-time').text(new Date().toLocaleString().split(', ')[1].replace(/:\d\d([ ap]|$)/, ' '));
+	$('.info-day').text('F Day');
+
+	// Move pointer
+	$('.timer-pointer').animate({
+		// left: ((vw * cellSpacing) + (timerWidth / maxTime) * (time % periodLength)) - (pointerWidth / 2)
+	});
+
+	// Move timer
+	$('.timer').animate({
+		// left: (vw * cellSpacing) - ((vw * cellWidth) * currentPeriod)
+		left: (vw * cellSpacing) - ((vw * cellWidth) * currentPeriod)
+	});
+
+	// Update columns
+	// ======= ADD Condition if not over max period time =======
+	// Reset column classes
+	$('.timer').children('.timer-column').removeClass('current');
+	$('.timer').children('.timer-column').removeClass('next');
+	$('.timer').children('.timer-column').css('opacity', '0.2');
+
+	// Set main column class
+	$('.timer').children(`.timer-column:eq(${currentPeriod})`).addClass('current');
+	$('.timer').children(`.timer-column:eq(${currentPeriod})`).css('opacity', '1');
+
+	// Show secondary columns
+	$('.timer').children(`.timer-column:eq(${currentPeriod + 1})`).addClass('next');
+	$('.timer').children(`.timer-column:eq(${currentPeriod + 1})`).css('opacity', '0.5');
+	if (currentPeriod - 1 !== -1) {
+		$('.timer').children(`.timer-column:eq(${currentPeriod - 1})`).css('opacity', '1');
+	}
+}
+
 function getCurrentSchedule() {
 	// get current day
+	let day = new Date().getDay();
 	// filter schedules by day
+	for (let i in schedules) {
+		if (schedules[i].days.includes(day)) {
+			scheduleIndex = i;
+			break;
+		}
+	}
 	// set current schedule variable
+	periods = schedules[scheduleIndex].data.times.length;
+	currentSchedule = schedules[scheduleIndex];
+	periodLength = 50*60;
 	// set timer columns to names
+	createColumns();
+}
+
+function createColumns() {
+	let template = $('#column-template').html();
+	for (let i in schedules[scheduleIndex].data.short) {
+		let hold = template.replace('{{name}}', schedules[scheduleIndex].data.short[i]);
+		$('.timer').append(hold);
+	}
+
+	timerInterval = setInterval(increaseTimer, 1000);
+	increaseTimer();
 }
 
 function getCurrentPeriod() {
+	// let remainingTime;
 	// get current time
-	// get current schedule variable
+
+	// DEV offset
+	let time = new Date();
+	time.setTime(time.getTime() + (timeOffset * 1000));
+
 	// find position in times array
-	// perform subtraction function
+	for (let i in currentSchedule.data.times) {
+		let periodTime = new Date();
+		periodTime.setHours(currentSchedule.data.times[i].split(':')[0]);
+		periodTime.setMinutes(currentSchedule.data.times[i].split(':')[1]);
+		periodTime.setSeconds(0);
+
+		if (time < periodTime) {
+			// perform subtraction function
+			remainingTime = periodTime - time;
+			currentPeriod = i;
+			break;
+
+			// console.table([periodTime.toLocaleString(), time.toLocaleString(), new Date(remainingTime).toLocaleString().split(', ')[1], currentPeriod	]);
+		}
+	}
 	// output time left
-	// set current period emphasis
+	$('.timer-pointer-label').text(parseTime(new Date(remainingTime)) + ' left');
+}
+
+function parseTime(date) {
+	let seconds = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds();
+	return date.getMinutes() + ':' + seconds;
 }
