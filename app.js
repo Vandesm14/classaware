c2835cf18e0e6e933f0eddf7fdbe47ff264a91ef
vandesm14 <vandesm14@gmail.com>
2019-11-12 19:12:13 -0500

    update route

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/public/pages/timer.html');
+	res.sendFile(__dirname + '/public/timer.html');
 });
 
 app.listen(5500, () => console.log('server started on port: ' + 5500));
